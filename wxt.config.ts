01be0c5edfd0f9053cba57ee54679dbcf04dce6c
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-16 18:16:41 -0700

    Add favicon Workaround

--- wxt.config.ts
+++ wxt.config.ts
@@ -83,6 +83,9 @@ export default defineConfig({
       console.log('build:manifestGenerated:', wxt.config.browser)
       if (manifest.action) manifest.action.default_icon = manifest.icons
       if (manifest.sidebar_action) manifest.sidebar_action.default_icon = manifest.icons
+      if (wxt.config.browser === 'firefox') {
+        manifest.permissions = manifest.permissions?.filter((p) => p !== 'favicon')
+      }
     },
     'build:done': async (wxt) => {
       const src = join(process.cwd(), 'node_modules/simple-icons/icons')
